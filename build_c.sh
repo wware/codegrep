e111355c13b423f45230d59f76a47e58bf608dc8
Will Ware <wware@alum.mit.edu>
2014-11-16 12:33:51 -0500

    Build for Mac and Linux, this needs work.

--- build_c.sh
+++ build_c.sh
@@ -1,6 +1,17 @@
-#!/bin/sh
+#!/bin/bash
 
-PYBASE=/usr/local/Cellar/python/2.7.8_1/Frameworks/Python.framework/Versions/2.7
+OS=$(uname -a | sed "s/ .*//")
 
-gcc -O3 -c -I${PYBASE}/include/python2.7 cgmodule.c
-gcc -shared -fPIC -o cgmodule.so -I${PYBASE}/lib/python2.7 cgmodule.o -lpython
+if [ "$OS" == "Linux" ]; then
+    PYBASE=/usr
+    LFLAGS="-L/usr/lib/x86_64-linux-gnu -lpython2.7"
+fi
+
+if [ "$OS" == "Darwin" ]; then
+    # Homebrew install of Python on the Mac
+    PYBASE=/usr/local/Cellar/python/2.7.8_1/Frameworks/Python.framework/Versions/2.7
+    LFLAGS="-L${PYBASE}/lib/python2.7 -lpython"
+fi
+
+gcc -O3 -c -fPIC -I${PYBASE}/include/python2.7 cgmodule.c
+gcc -shared -fPIC -o cgmodule.so ${LFLAGS} cgmodule.o -lpython2.7
